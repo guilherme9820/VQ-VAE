62460511bb2b70290302fd5a8070f995a6d28611
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2020-02-19 19:15:16 -0300

    Handler for notMNIST dataset

---
+++ DataHandler.py
@@ -0,0 +1,64 @@
+import numpy as np
+import tarfile
+import sys
+import os
+
+URL = 'https://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+np.random.seed(133)
+
+
+def download_progress_hook(count, blockSize, totalSize):
+    """A hook to report the progress of a download. This is mostly intended for users with
+    slow internet connections. Reports every 5% change in download progress.
+    """
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+        last_percent_reported = percent
+
+
+def maybe_download(data_root, filename, expected_bytes, force=False):
+    """Download a file if not present, and make sure it's the right size."""
+    dest_filename = os.path.join(data_root, filename)
+    if force or not os.path.exists(dest_filename):
+        print('Attempting to download:', filename)
+        filename, _ = urlretrieve(URL + filename, dest_filename, reporthook=download_progress_hook)
+        print('\nDownload Complete!')
+    statinfo = os.stat(dest_filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', dest_filename)
+    else:
+        raise Exception(
+            'Failed to verify ' + dest_filename + '. Can you get to it with a browser?')
+    return dest_filename
+
+
+def maybe_extract(data_root, filename, num_classes, force=False):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    if os.path.isdir(root) and not force:
+        # You may override by setting force=True.
+        print('%s already present - Skipping extraction of %s.' % (root, filename))
+    else:
+        print('Extracting data for %s. This may take a while. Please wait.' % root)
+        tar = tarfile.open(filename)
+        sys.stdout.flush()
+        tar.extractall(data_root)
+        tar.close()
+    data_folders = [
+        os.path.join(root, d) for d in sorted(os.listdir(root))
+        if os.path.isdir(os.path.join(root, d))]
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+    print(data_folders)
+    return data_folders
